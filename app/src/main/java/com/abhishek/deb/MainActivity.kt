9c1f88d8ad7c552afdf73483027ee4735fd65251
abhishekDeb-2003 <72614033+abhishekDeb-2003@users.noreply.github.com>
2020-10-12 10:42:48 +0530

    Update MainActivity.kt

--- app/src/main/java/com/abhishek/deb/MainActivity.kt
+++ app/src/main/java/com/abhishek/deb/MainActivity.kt
@@ -71,17 +71,5 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         }
-
-        val drawableRes = when (nextInt(6) + 1) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.empty_dice
-        }
-
-        diceView.setImageResource(drawableRes)
     }
 }
